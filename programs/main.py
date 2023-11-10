cbff718be4bcd05d6be4ba9db5b2faede54a7b82
1qasxcvgy8ikm <110016829+1qasxcvgy8ikm@users.noreply.github.com>
2023-11-10 17:26:50 +0300

    ping pong, v1.2

---
+++ programs/main.py
@@ -0,0 +1,74 @@
+from pygame import *
+from random import randint
+from time import time as timer
+
+img_back = 'background.jpg'
+img_racket = 'racket.png'
+
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+
+
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+
+
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+#класс главного игрока
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+    def update_1(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+    def update_2(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 150:
+            self.rect.y += self.speed
+    
+
+
+#создаем окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+racket_1 = Player(img_racket,30,200,50,150,10)
+racket_2 = Player(img_racket,620,200,50,150,10)
+
+
+#переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+#основной цикл игры:
+run = True #флаг сбрасывается кнопкой закрытия окна
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+
+    window.blit(background,(0,0))
+
+    racket_1.update_1()
+    racket_2.update_2()
+    racket_1.reset()
+    racket_2.reset()
+
+    display.update()
+    time.delay(30)
